18df6e0039ab4037d42a96d2987ec004dc2f652b
Emile <moraesemile@gmail.com>
2021-03-07 15:39:01 -0300

    Projeto: Netflix - Clone

---
+++ hello.js
@@ -0,0 +1,2 @@
+var nome = prompt('Qual é o seu nome?');
+console.log('Hello, ' + nome + '!');
---
+++ teste.js
@@ -0,0 +1,5 @@
+const dino = document.querySelector('.dino');
+
+document.addEventListener('keydown', () => {
+  console.log("'pressionou uma tecla'") ; 
+});
